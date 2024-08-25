8f38e38ce5e98edc2941f73f03d2a4e85c384dec
Andrew <andrewchau1993@gmail.com>
2024-08-24 19:11:57 -0700

    corrected code

--- controllers/logEntry.js
+++ controllers/logEntry.js
@@ -43,7 +43,7 @@ router.use(verifyToken);
 router.post('/', async (req, res) => {
   try {
     req.body.author = req.user._id
-    const log = await logEntry.create(req.body);
+    const log = await logEntry.create(req.body)
     log._doc.author = req.user
     res.status(201).json(log)
   } catch (err) {
